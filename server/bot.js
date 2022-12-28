f63dd533e27a2dc60ed82043770b0f26bcf5b1bd
Nour Eldin <Nourraaft@gmail.com>
2022-12-28 10:35:09 +0200

    Update

--- server/bot.js
+++ server/bot.js
@@ -2,47 +2,44 @@ import TeleBot from "telebot";
 import { openAiDate } from "./openai.js";
 import { connect } from "./config.js";
 import Model from "./mongoose.js";
-export const botTele = () => {
+export const botTele = async () => {
    connect();
    const token = process.env.TELEBOT_TOKEN;
    const bot = new TeleBot(token);
-   bot.on("/start", async (msg) => {
-      Promise.race([
-         new Promise((resolve, reject) => {
-            try {
-               setTimeout(async () => {
-                  resolve(`Hello, ${msg.from.first_name}!
-   Enter the message and the OpenAI bot will answer you!
-   Just for experience, the answers may be different and not correct.\n
-   This bot uses OpenAI's GPT-3 model (https://openai.com/api/). Messages may be recorded.
-   for developer : @Noureldin13`);
-               }, 2000);
-            } catch (error) {
-               reject(error);
-            }
-         }),
-      ])
-         .then(async (data) => {
-            return await bot.sendMessage(msg.chat.id, await data, {
-               replyToMessage: msg.chat.type == "supergroup" && msg.message_id,
+   await bot.on("/start", async (msg) => {
+      new Promise(async (_) => {
+         try {
+            bot.sendMessage(
+               msg.chat.id,
+               `Hello, ${msg.from.first_name}!
+Enter the message and the OpenAI bot will answer you!
+Just for experience, the answers may be different and not correct.\n
+This bot uses OpenAI's GPT-3 model (https://openai.com/api/). Messages may be recorded.
+for developer : @Noureldin13`,
+               {
+                  replyToMessage: msg.message_id,
+               }
+            );
+         } catch (error) {
+            bot.sendMessage(msg.chat.id, JSON.stringify(error), {
+               replyToMessage: msg.message_id,
             });
-         })
-         .catch(async (error) => {
-            return await bot.sendMessage(msg.chat.id, JSON.stringify(error), {
-               replyToMessage: msg.chat.type == "supergroup" && msg.message_id,
-            });
-         });
+         }
+      });
    });
 
-   bot.on("*", async (msg) => {
+   await bot.on("text", async (msg) => {
       if (msg.text == "/start") return;
-      const findUser = await Model.findById(msg.chat.id)
-      if (findUser) {   
-         await Model.updateMany({ _id: findUser._id }, {
-            $push: {
-               messages: msg.text,
+      const findUser = await Model.findById(msg.chat.id);
+      if (findUser) {
+         await Model.updateMany(
+            { _id: findUser._id },
+            {
+               $push: {
+                  messages: msg.text,
+               },
             }
-         })
+         );
       } else {
          await Model.insertMany({
             _id: msg.chat.id,
@@ -51,70 +48,42 @@ export const botTele = () => {
             messages: [msg.text],
          });
       }
-      console.log(JSON.stringify([...findUser.messages, msg.text], null, 2));
-      if (msg.chat.type == "supergroup") {
-         bot.on(/^\/chat (.+)$/, async (msg, props) => {
-            const prompt = props.match[1];
-            if (!prompt) return;
-            Promise.race([
-               new Promise((resolve, reject) => {
-                  try {
-                     setTimeout(async () => {
-                        await bot.sendMessage(msg.chat.id, "Wait ...", {
-                           replyToMessage:
-                              msg.chat.type == "supergroup" && msg.message_id,
-                        });
-                        resolve(openAiDate(prompt));
-                     }, 1000);
-                  } catch (error) {
-                     reject(error);
-                  }
-               }),
-            ])
-               .then(async (data) => {
-                  return await bot
-                     .sendMessage(msg.chat.id, await data, {
-                        replyToMessage: msg.message_id,
-                     })
-                     .catch((err) => console.log(err));
-               })
-               .catch(async (error) => {
-                  return await bot.sendMessage(
-                     msg.chat.id,
-                     JSON.stringify(error),
-                     {
-                        replyToMessage: msg.message_id,
-                     }
-                  );
-               })
-               .catch((err) => console.log(err));
-         });
-      } else if ((msg.chat.type = "private")) {
+      if (msg.chat.type === "private") {
          const prompt = msg.text.trim();
-         Promise.race([
-            new Promise((resolve, reject) => {
-               try {
-                  setTimeout(async () => {
-                     await bot.sendMessage(msg.chat.id, "Wait ..");
-                     resolve(openAiDate(prompt));
-                  }, 1000);
-               } catch (error) {
-                  reject(error);
-               }
-            }),
-         ])
-            .then(async (data) => {
-               return await bot
-                  .sendMessage(msg.chat.id, await data)
-                  .catch((err) => console.log(err));
-            })
-            .catch(async (error) => {
-               return await bot
-                  .sendMessage(msg.chat.id, JSON.stringify(error))
-                  .catch((err) => console.log(err));
-            });
+         new Promise(async (_) => {
+            try {
+               bot.sendMessage(msg.chat.id, "Wait ..", {
+                  replyToMessage: msg.message_id,
+               });
+               bot.sendMessage(msg.chat.id, await openAiDate(prompt), {
+                  replyToMessage: msg.message_id,
+               });
+            } catch (error) {
+               bot.sendMessage(msg.chat.id, JSON.stringify(error), {
+                  replyToMessage: msg.message_id,
+               });
+            }
+         });
       }
    });
+   await bot.on(/^\/chat (.+)$/, async (msg, props) => {
+      const prompt = msg.text.replace(/\/chat/i, "").trim();
+      if (!prompt) return;
+      new Promise(async (_) => {
+         try {
+            bot.sendMessage(msg.chat.id, "Wait ..", {
+               replyToMessage: msg.message_id,
+            });
+            bot.sendMessage(msg.chat.id, await openAiDate(prompt), {
+               replyToMessage: msg.message_id,
+            });
+         } catch (error) {
+            bot.sendMessage(msg.chat.id, JSON.stringify(error), {
+               replyToMessage: msg.message_id,
+            });
+         }
+      });
+   });
 
-   bot.start();
+   await bot.start();
 };
