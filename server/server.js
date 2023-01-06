516d45d08c3f871b3520508ba84dbfed41704a4c
Nour Eldin <Nourraaft@gmail.com>
2023-01-06 14:45:39 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,10 +3,11 @@ import * as dotenv from "dotenv";
 import cors from "cors";
 import { botTele } from "./bot.js";
 import { openAiDate } from "./openai.js";
+import { connect } from "./config.js";
 dotenv.config();
 const PORT = process.env.PORT_SERVER || 5000;
 botTele();
-
+connect()
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -19,9 +20,21 @@ app.get("/", (req, res) => {
 
 app.post("/", async (req, res) => {
    try {
+       const find = await ViewsModel.findOne({ page: "home" });
+      if (find.length === 0) {
+         await ViewsModel({
+            page: "home",
+            views: 1,
+         }).save();
+      }
+      await ViewsModel.updateMany(
+         { page: "home" },
+         { $set: { views: find.views + 1 } }
+      );
       const prompt = await req.body.prompt;
-      res.status(200).json({
+      return res.status(200).json({
          bot: await openAiDate(prompt),
+         views: find.views,
       });
    } catch (error) {
       return res.status(500).json({ message: error.message });
